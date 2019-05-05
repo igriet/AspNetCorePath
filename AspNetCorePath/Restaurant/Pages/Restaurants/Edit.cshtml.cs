6e727c3170082e1148a2732b55ff8454c45cf611
16R137 <igriet.gonzalez@outlook.com>
2019-05-05 00:39:33 -0500

    Create edit view

---
+++ AspNetCorePath/Restaurant/Pages/Restaurants/Edit.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Restaurant.Data;
+
+namespace Restaurant.Pages.Restaurants
+{
+    public class EditModel : PageModel
+    {
+        private IRestaurantData _restaurantData;
+
+        public Core.Restaurant Restaurant { get; set; }
+
+        public EditModel(IRestaurantData data)
+        {
+            _restaurantData = data;
+        }
+
+        public IActionResult OnGet(int restaurantId)
+        {
+            Restaurant = _restaurantData.GetById(restaurantId);
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+    }
+}
