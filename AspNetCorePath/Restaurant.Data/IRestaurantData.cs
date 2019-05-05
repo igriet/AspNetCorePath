6f061eba4e3a4b11c91824b1360ed52dee50085e
16R137 <igriet.gonzalez@outlook.com>
2019-05-04 20:33:04 -0500

    creation core and data
    Creation of core and data project

---
+++ AspNetCorePath/Restaurant.Data/IRestaurantData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Core.Restaurant> GetAll();
+    }
+}
---
+++ AspNetCorePath/Restaurant.Data/InMemoryRestaurantData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Restaurant.Core;
+
+namespace Restaurant.Data
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        public readonly List<Core.Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Core.Restaurant>() {
+                new Core.Restaurant { Id = 1, Name = "Domino's Pizza", Location = "Seattle", Cuisine = CuisineType.Italian },
+                new Core.Restaurant { Id = 2, Name = "Apu's Home", Location = "Delhi", Cuisine = CuisineType.Indian },
+                new Core.Restaurant { Id = 3, Name = "Chon&Chano", Location = "CDMX", Cuisine = CuisineType.Mexican },
+                new Core.Restaurant { Id = 4, Name = "El Pechugon", Location = "Tlaquepaque", Cuisine = CuisineType.None }
+            };
+        }
+
+        public IEnumerable<Core.Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
