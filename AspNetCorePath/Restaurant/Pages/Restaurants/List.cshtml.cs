7348ddfa31374108a4ca8585fb1aca89443e7430
16R137 <igriet.gonzalez@outlook.com>
2019-05-04 19:34:09 -0500

    message with property
    message send setting the property in the view model and also setting a configuration variable in the appsettings.json

---
+++ AspNetCorePath/Restaurant/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace Restaurant.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        private readonly IConfiguration _configuration;
+
+        public string Message { get; set; }
+
+        public ListModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void OnGet()
+        {
+            Message = _configuration["Message"];//"Hola desde cshtml";
+        }
+    }
+}
