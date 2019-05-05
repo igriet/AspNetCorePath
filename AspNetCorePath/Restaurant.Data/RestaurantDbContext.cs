24b254cf6274213909666363300623cf936da40c
16R137 <igriet.gonzalez@outlook.com>
2019-05-05 15:25:30 -0500

    create connection string in order to CodeFirst

--- AspNetCorePath/Restaurant.Data/RestaurantDbContext.cs
+++ AspNetCorePath/Restaurant.Data/RestaurantDbContext.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Restaurant.Data
 {
-    class RestaurantDbContext
+    public class RestaurantDbContext : DbContext
     {
+        public DbSet<Core.Restaurant> Restaurant { get; set; }
     }
 }
