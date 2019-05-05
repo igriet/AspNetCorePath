1f38320711c1148d7217894dc294a8ada99e53ee
16R137 <igriet.gonzalez@outlook.com>
2019-05-05 13:20:07 -0500

    tempdata is deprecated to use it as data annotation

--- AspNetCorePath/Restaurant/Pages/Restaurants/Detail.cshtml.cs
+++ AspNetCorePath/Restaurant/Pages/Restaurants/Detail.cshtml.cs
@@ -10,8 +10,6 @@ namespace Restaurant.Pages.Restaurants
 {
     public class DetailModel : PageModel
     {
-        [TempData]
-        public string Message { get; set; }
         public Core.Restaurant Restaurant { get; set; }
 
         private IRestaurantData _restaurantData;
