8ebce1c66016c86b6013a246a9938519baa908c2
igriet_job <igriet.gonzalez@outlook.com>
2019-05-06 13:39:33 -0500

    create code behind of delete feature

---
+++ AspNetCorePath/Restaurant/Pages/Restaurants/Delete.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Restaurant.Data;
+
+namespace Restaurant.Pages.Restaurants
+{
+    public class DeleteModel : PageModel
+    {
+        private IRestaurantData _restaurantData;
+
+        public Core.Restaurant Restaurant { get; set; }
+
+        public DeleteModel(IRestaurantData data)
+        {
+            _restaurantData = data;
+        }
+
+        public IActionResult OnGet(int restaurantId)
+        {
+            Restaurant = _restaurantData.GetById(restaurantId);
+            if (Restaurant == null)
+                return RedirectToPage("./NotFound");
+
+            return Page();
+        }
+
+        public IActionResult OnPost(int restaurantId)
+        {
+            var restaurant = _restaurantData.Delete(restaurantId);
+            _restaurantData.Commit();
+
+            if (restaurant == null)
+                return RedirectToPage("./NotFound");
+            TempData["Message"] = $"{restaurant.Name} deleted";
+            return RedirectToPage("./List");
+        }
+    }
+}
