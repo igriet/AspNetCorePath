211268902603bcd104572883f7ebe3072745db25
igriet_job <igriet.gonzalez@outlook.com>
2019-05-07 12:12:07 -0500

    create simple logging details in razor page

--- AspNetCorePath/Restaurant/Pages/Restaurants/List.cshtml.cs
+++ AspNetCorePath/Restaurant/Pages/Restaurants/List.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Restaurant.Data;
 
 namespace Restaurant.Pages.Restaurants
@@ -13,20 +14,25 @@ namespace Restaurant.Pages.Restaurants
     {
         private readonly IConfiguration _configuration;
         private readonly IRestaurantData _restaurantData;
+        private readonly ILogger<ListModel> _logger;
 
         public string Message { get; set; }
         public IEnumerable<Core.Restaurant> Restaurants { get; set; }
         [BindProperty(SupportsGet =true)]
         public string SearchTerm { get; set; }
 
-        public ListModel(IConfiguration configuration,IRestaurantData data)
+        public ListModel(IConfiguration configuration,
+                            IRestaurantData data,
+                            ILogger<ListModel> logger)
         {
             _configuration = configuration;
             _restaurantData = data;
+            _logger = logger;
         }
 
         public void OnGet()
         {
+            _logger.LogError("Excecuting List Get");
             Message = _configuration["Message"];//"Hola desde cshtml";
             Restaurants = _restaurantData.GetRestaurantByName(SearchTerm);
         }
