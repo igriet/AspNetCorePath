6fd06e2f6483863a0e54f612a8e9eaf6559fb441
igriet_job <igriet.gonzalez@outlook.com>
2019-05-09 16:09:33 -0500

    create mock and poco

--- AspNetCorePath/BethanysPieShop/Startup.cs
+++ AspNetCorePath/BethanysPieShop/Startup.cs
@@ -15,6 +15,7 @@ namespace BethanysPieShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IPieRepository, MockPieRepository>();
             services.AddMvc();
         }
 
